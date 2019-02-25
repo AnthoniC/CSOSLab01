80371c6320f55517df067e1c3f1847c3ea10490a
Anthoni <anthoni@anthonis-imac.local>
2019-02-24 21:43:58 -0600

    Initial Commit

---
+++ lab01-acaravello/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lab01-acaravello
+//
+//  Created by Anthoni on 2/24/19.
+//  Copyright © 2019 Anthoni. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
